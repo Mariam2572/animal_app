0de55638c45ca06b5f47d210f9bd633d5d0d97ed
‪Mariam Kamel‬‏ <122682793+Mariam2572@users.noreply.github.com>
2025-10-16 15:14:50 +0300

    - Implemented the main application window and message handling. - Integrated Flutter plugin registration for secure storage. - Added resource files including icons and application manifest. - Configured project settings for high DPI support and dark mode. - Established console output redirection for debugging. - Included necessary utility functions for command line argument handling.

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:animal_app/Core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        theme: AppTheme.theme,
+      ),
+    );
+  }
+}
+
+// api key: live_LJqbxLYW43N9hXFna6P3ss9wKlrIMyoRN6oRXtw416igkcoG2kXkFCNRxg3JMt1k
